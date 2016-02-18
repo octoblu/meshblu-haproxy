efbf37061a094be5a332ddb4dfe21a7b8b3a3673
Erik Wilson <Erik.E.Wilson@gmail.com>
2016-02-17 18:17:28 -0700

    all of the acls

--- haproxy.cfg-dev.sh
+++ haproxy.cfg-dev.sh
@@ -17,8 +17,19 @@ frontend http
   bind *:80
   mode http
 
+  acl is-delete method DELETE
+  acl use-meshblu-http path_reg ^/devices/[^/]+/subscriptions$
+  acl use-meshblu-http-delete-tokens path_reg ^/devices/[^/]+/tokens$
+
   acl use-meshblu-http path_reg ^/v3/devices/[^/]+$
+  acl use-meshblu-http path_reg ^/search/devices$
+
+  acl use-meshblu-http path_beg /messages
+  acl use-meshblu-http path_beg /v2/whoami
+
+  use_backend meshblu-http if is-delete use-meshblu-http-delete-tokens
   use_backend meshblu-http if use-meshblu-http
+
   default_backend meshblu-old
 
 backend meshblu-old
@@ -30,6 +41,5 @@ backend meshblu-http
   option forwardfor
   server meshblu-http meshblu-messages.octoblu.dev:80 cookie meshblu-http
   http-request set-header Host meshblu-messages.octoblu.dev
-
 
 EOF
