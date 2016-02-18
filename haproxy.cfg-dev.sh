f801c1b4e6b0785ea0473999392470e2777e7a97
redaphid <a@octoblu.com>
2016-02-17 17:04:36 -0700

    omg. one conditional route

--- haproxy.cfg-dev.sh
+++ haproxy.cfg-dev.sh
@@ -8,19 +8,28 @@ defaults
   mode    http
   option  httplog
   option  dontlognull
-  timeout connect 5000
   timeout client  50000
-  timeout server  50000
+  timeout queue 5000
+  timeout server 86400000
+  timeout connect 86400000
 
-frontend localnodes
+frontend http
   bind *:80
   mode http
-  default_backend nodes
 
-backend nodes
-  mode http
+  acl use-meshblu-http path_reg ^/v3/devices/[^/]+$
+  use_backend meshblu-http if use-meshblu-http
+  default_backend meshblu-old
+
+backend meshblu-old
   option forwardfor
-  server web01 meshblu-old.octoblu.dev:80 check
+  server meshblu-old meshblu-old.octoblu.dev:80 cookie meshblu-old
   http-request set-header Host meshblu-old.octoblu.dev
+
+backend meshblu-http
+  option forwardfor
+  server meshblu-http meshblu-messages.octoblu.dev:80 cookie meshblu-http
+  http-request set-header Host meshblu-messages.octoblu.dev
+
 
 EOF
