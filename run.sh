3c9215e4f68adba351d2a13d04ed0666a70e79de
Jade Meskill <jade.meskill@gmail.com>
2016-03-09 16:34:03 -0700

    handle sigterm

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$SERVERS" ]; then
   echo "SERVERS not found, cowardly refusing to do anything"
@@ -10,5 +10,16 @@ if [ -z "$SERVER_NAME" ]; then
   exit 1
 fi
 
+function _term {
+  echo "Caught SIGTERM signal!"
+  kill -TERM "$child" 2>/dev/null
+}
+
+trap _term SIGTERM
+
 ./haproxy.cfg.sh > /usr/local/etc/haproxy/haproxy.cfg
-haproxy -f /usr/local/etc/haproxy/haproxy.cfg -L $SERVER_NAME
+haproxy -f /usr/local/etc/haproxy/haproxy.cfg -L $SERVER_NAME &
+
+child=$!
+echo "Waiting for child process: $child"
+wait "$child"
