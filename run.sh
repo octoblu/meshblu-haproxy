e5e27a599ea673679fc9b5cc7058bb30c6369d90
Peter DeMartini <thepeterdemartini@gmail.com>
2016-08-03 10:41:31 -0700

    v5.0.0 added support for the cluster domain

--- run.sh
+++ run.sh
@@ -9,6 +9,15 @@ assert_password() {
   fi
 }
 
+assert_cluster_domain() {
+  local cluster_domain="$1"
+
+  if [ -z "$cluster_domain" ]; then
+    echo "CLUSTER_DOMAIN not found, cowardly refusing to do anything"
+    exit 1
+  fi
+}
+
 run_haproxy() {
   haproxy -f /usr/local/etc/haproxy/haproxy.cfg &
   child=$!
@@ -18,15 +27,18 @@ run_haproxy() {
 
 write_haproxy() {
   local password="$1"
+  local cluster_domain="$2"
 
-  cat ./haproxy.cfg | sed s/{{PASSWORD}}/${password}/g > /usr/local/etc/haproxy/haproxy.cfg
+  cat ./haproxy.cfg | sed s/{{PASSWORD}}/${password}/g | sed s/{{CLUSTER_DOMAIN}}/${cluster_domain}/g > /usr/local/etc/haproxy/haproxy.cfg
 }
 
 main(){
   local password="${PASSWORD}"
   assert_password "${password}"
+  local cluster_domain="${CLUSTER_DOMAIN}"
+  assert_cluster_domain "${cluster_domain}"
 
-  write_haproxy "${password}"
+  write_haproxy "${password}" "${cluster_domain}"
   run_haproxy
 }
 
